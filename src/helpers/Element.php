edbf8742111f676c5c49258e89350303bdd64933
Antonio Ramirez <amigo.cobos@gmail.com>
2015-02-12 13:08:24 +0100

    update phpDocs + fixes + additions

--- src/helpers/Element.php
+++ src/helpers/Element.php
@@ -8,7 +8,6 @@ namespace dosamigos\semantic\helpers;
 
 use Yii;
 use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
 
 /**
  * Element provides a set of static methods for generating semantic ui elements HTML tags
@@ -35,8 +34,8 @@ class Element
         $tag = ArrayHelper::remove($options, 'tag', 'div');
         Ui::addCssClasses($options, ['ui', 'button']);
         return strcasecmp($tag, 'button') === 0
-            ? Html::button($content, $options)
-            : Html::tag($tag, $content, $options);
+            ? Ui::button($content, $options)
+            : Ui::tag($tag, $content, $options);
     }
 
     /**
@@ -72,10 +71,10 @@ class Element
         Ui::addCssClasses($hOptions, ['hidden', 'content']);
         Ui::addCssClasses($vOptions, ['visible', 'content']);
         $lines = [];
-        $lines[] = Html::tag('div', $hidden, $hOptions);
-        $lines[] = Html::tag('div', $visible, $vOptions);
+        $lines[] = Ui::tag('div', $hidden, $hOptions);
+        $lines[] = Ui::tag('div', $visible, $vOptions);
 
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
 
     }
 
@@ -114,7 +113,7 @@ class Element
             $label = ArrayHelper::remove($button, 'label', Yii::t('dosamigos/semantic/helpers', 'label'));
             $lines[] = static::button($label, $button);
         }
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
     }
 
     /**
@@ -135,14 +134,14 @@ class Element
         Ui::addCssClasses($options, ['ui', 'buttons']);
         Ui::addCssClasses($lOptions, ['ui', 'button']);
         Ui::addCssClasses($rOptions, ['ui', 'button']);
-        Html::addCssClass($oOptions, 'or');
+        Ui::addCssClass($oOptions, 'or');
 
         $lines = [];
         $lines[] = static::button($leftLabel, $lOptions);
-        $lines[] = Html::tag('div', '', $oOptions);
+        $lines[] = Ui::tag('div', '', $oOptions);
         $lines[] = static::button($rightLabel, $rOptions);
 
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
     }
 
     /**
@@ -156,7 +155,7 @@ class Element
     public static function icon($glyph, $options = [])
     {
         Ui::addCssClasses($options, ['icon', $glyph]);
-        return Html::tag('i', '', $options);
+        return Ui::tag('i', '', $options);
     }
 
     /**
@@ -171,7 +170,7 @@ class Element
     {
         Ui::addCssClasses($options, ['ui', 'divider']);
 
-        return Html::tag('div', $label, $options);
+        return Ui::tag('div', $label, $options);
     }
 
     /**
@@ -185,7 +184,7 @@ class Element
     public static function flag($country, $options = [])
     {
         Ui::addCssClasses($options, ['flag', $country]);
-        return Html::tag('i', '', $options);
+        return Ui::tag('i', '', $options);
     }
 
     /**
@@ -199,7 +198,7 @@ class Element
     public static function avatar($src, $options = [])
     {
         Ui::addCssClasses($options, ['ui', 'avatar', 'image']);
-        return Html::img($src, $options);
+        return Ui::img($src, $options);
     }
 
     /**
@@ -214,7 +213,7 @@ class Element
     {
         $tag = ArrayHelper::remove($options, 'tag', 'div');
         Ui::addCssClasses($options, ['ui', 'label']);
-        return Html::tag($tag, $content, $options);
+        return Ui::tag($tag, $content, $options);
     }
 
     /**
@@ -228,7 +227,7 @@ class Element
      */
     public static function labelDetail($content, $detail, $options = [])
     {
-        $detail = Html::tag('div', $detail, ['class' => 'detail']);
+        $detail = Ui::tag('div', $detail, ['class' => 'detail']);
         return static::label($content . $detail, $options);
     }
 
@@ -242,7 +241,7 @@ class Element
      */
     public static function labelTag($content, $options = [])
     {
-        Html::addCssClass($options, 'tag');
+        Ui::addCssClass($options, 'tag');
         return static::label($content, $options);
     }
 
@@ -256,7 +255,7 @@ class Element
      */
     public static function labelRibbon($content, $options = [])
     {
-        Html::addCssClass($options, 'ribbon');
+        Ui::addCssClass($options, 'ribbon');
         return static::label($content, $options);
     }
 
@@ -277,14 +276,20 @@ class Element
             $content = ArrayHelper::remove($label, 'content');
             $lines[] = static::label($content, $label);
         }
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
     }
 
     /**
-     * @param $items
-     * @param array $options
+     * Generates a group list @see http://semantic-ui.com/elements/list.html
      *
-     * @return string
+     * @param array $items the items to render on the group. Each item configuration supports these special options:
+     * - icon, string, optional, the item's icon
+     * - image, string, optional, the item's image
+     * - header, string, optional, the item's header
+     * - description, string, option, the description of the item
+     * @param array $options the tag options in terms of name-value pairs of the layer containing the list group
+     *
+     * @return string the generated list
      */
     public static function groupList($items, $options = [])
     {
@@ -300,27 +305,29 @@ class Element
                 $header = ArrayHelper::remove($item, 'header');
                 $description = ArrayHelper::remove($item, 'description');
                 if ($description) {
-                    $description = Html::tag('div', $description, ['class' => 'description']);
+                    $description = Ui::tag('div', $description, ['class' => 'description']);
                 }
 
                 $content = [];
                 $content[] = $icon;
                 $content[] = $image;
                 if ($header || $description) {
-                    $content[] = Html::tag('div', $header . $description, ['class' => 'content']);
+                    $content[] = Ui::tag('div', $header . $description, ['class' => 'content']);
                 }
-                $lines[] = Html::tag('div', implode("\n", $content), $item);
+                $lines[] = Ui::tag('div', implode("\n", $content), $item);
             }
         }
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
     }
 
     /**
-     * @param $visibleSrc
-     * @param $hiddenSrc
-     * @param array $options
+     * Generates an image reveal element @see http://semantic-ui.com/elements/reveal.html
      *
-     * @return string
+     * @param string $visibleSrc the image source to be visible when render
+     * @param string $hiddenSrc the image source to be hidden when render
+     * @param array $options the tag options in terms of name-value pairs.
+     *
+     * @return string the generated reveal tag
      */
     public static function imgReveal($visibleSrc, $hiddenSrc, $options = [])
     {
@@ -328,52 +335,84 @@ class Element
 
         $visibleOptions = ArrayHelper::remove($options, 'visible', []);
         Ui::addCssClasses($visibleOptions, ['visible', 'content']);
-        $lines[] = Html::img($visibleSrc, $visibleOptions);
+        $lines[] = Ui::img($visibleSrc, $visibleOptions);
 
         $hiddenOptions = ArrayHelper::remove($options, 'hidden', []);
         Ui::addCssClasses($hiddenOptions, ['hidden', 'content']);
-        $lines[] = Html::img($hiddenSrc, $hiddenOptions);
+        $lines[] = Ui::img($hiddenSrc, $hiddenOptions);
 
         Ui::addCssClasses($options, ['ui', 'reveal']);
-        return Html::tag('div', implode("\n", $lines), $options);
+        return Ui::tag('div', implode("\n", $lines), $options);
     }
 
     /**
-     * @param $items
-     * @param array $options
+     * Renders a segment @see http://semantic-ui.com/elements/segment.html
      *
-     * @return string
+     * @param string $content the content of the segment
+     * @param array $options the tag options in terms of name-value pairs
+     *
+     * @return string the generated segment tag
+     */
+    public static function segment($content, $options = [])
+    {
+        Ui::addCssClasses($options, ['ui', 'segment']);
+        return Ui::tag('div', $content, $options);
+    }
+
+    /**
+     * Renders a step element item @see http://semantic-ui.com/elements/step.html
+     *
+     * @param string $content the content of the item
+     * @param array $options the tag options in terms of name-value pairs
+     *
+     * @return string the generated step item tag
+     */
+    public static function step($content, $options = [])
+    {
+        Ui::addCssClass($options, 'step');
+        $tag = ArrayHelper::remove($options, 'tag', 'div');
+        return Ui::tag($tag, $content, $options);
+    }
+
+    /**
+     * Generates step element @see http://semantic-ui.com/elements/step.html
+     *
+     * @param array $items the configuration of the steps. The configuration options may include:
+     * - tag, string, optional, the layer tag. Defaults to 'div'
+     * - icon, string, optional, the icon to display on the step
+     * - title, string, optional, the title of the step
+     * - description, string, optional, the description of the step
+     * @param array $options the tag options in terms of name-value pairs
+     *
+     * @return string the generated step tag
      */
     public static function steps($items, $options = [])
     {
         $steps = array_map(
             function ($item) {
-                $tag = ArrayHelper::remove($item, 'tag', 'div');
                 $icon = ArrayHelper::remove($item, 'icon', '');
                 $title = ArrayHelper::remove($item, 'title', '');
                 $description = ArrayHelper::remove($item, 'description');
 
                 if (!empty($title)) {
-                    $title = Html::tag('div', $title, ['class' => 'title']);
+                    $title = Ui::tag('div', $title, ['class' => 'title']);
                 }
                 if (!empty($description)) {
-                    $description = Html::tag('div', $description, ['class' => 'description']);
+                    $description = Ui::tag('div', $description, ['class' => 'description']);
                 }
                 if (!empty($icon)) {
                     $icon = static::icon($icon);
-                    $content = Html::tag('div', $title . $description, ['class' => 'content']);
+                    $content = Ui::tag('div', $title . $description, ['class' => 'content']);
                 } else {
                     $content = $title . $description;
                 }
-
-                Html::addCssClass($item, 'step');
-                return Html::tag($tag, $icon . $content, $item);
+                return static::step($icon . $content, $item);
             },
             $items
         );
 
         Ui::addCssClasses($options, ['ui', 'steps']);
 
-        return Html::tag('div', implode("\n", $steps), $options);
+        return Ui::tag('div', implode("\n", $steps), $options);
     }
 }
