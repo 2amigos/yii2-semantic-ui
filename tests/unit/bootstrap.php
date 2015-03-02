571a579ccff49e4875a559fe78da100f1b38aac7
Antonio Ramirez <amigo.cobos@gmail.com>
2015-03-02 09:14:30 +0100

    remove commented code

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -7,16 +7,6 @@
  * @link http://www.ramirezcobos.com/
  * @link http://www.2amigos.us/
  */
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'test');
-//require(__DIR__ . '/../vendor/autoload.php');
-//require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-//Yii::setAlias('@tests', __DIR__);
-//new \yii\console\Application([
-//    'id' => 'testApp',
-//    'basePath' => __DIR__,
-//]);
-
 error_reporting(-1);
 define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_DEBUG', true);
